f8d22d3d1a6f297f772f8614f3d6c04e1361c998
Paul <paul@paulhhowells.net>
2018-03-12 13:01:29 +0000

    Renamed thing to things

--- tests/unit/models/thing-test.js
+++ tests/unit/models/thing-test.js
@@ -2,13 +2,13 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { run } from '@ember/runloop';
 
-module('Unit | Model | things', function(hooks) {
+module('Unit | Model | thing', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
     let store = this.owner.lookup('service:store');
-    let model = run(() => store.createRecord('things', {}));
+    let model = run(() => store.createRecord('thing', {}));
     assert.ok(model);
   });
 });
