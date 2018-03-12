19d7aa88bf0821c8acd6261222adb833d2833be1
Paul <paul@paulhhowells.net>
2018-03-12 16:02:06 +0000

    Added plan & things models to the index route model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  model () {
+    return RSVP.hash({
+      plan: this.get('store')
+        .queryRecord('plan', {})
+        .then(function (plan) {
+
+          console.log('id', plan.id);
+          console.log('days', plan.get('days'));
+
+          return plan;
+        }),
+      things: this.get('store').findAll('thing'),
+    });
+  }
 });
