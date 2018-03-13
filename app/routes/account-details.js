2a6716c486f4bff8773c030adf9f7a242082da0e
Paul <paul@paulhhowells.net>
2018-03-13 15:14:43 +0000

    Added plan model to account-details route

--- app/routes/account-details.js
+++ app/routes/account-details.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return this.get('store')
+      .queryRecord('plan', {})
+      .then(function (plan) {
+        return plan;
+      });
+  }
 });
