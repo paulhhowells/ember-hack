ab165f1e2101588de86cee028f2d8ec8e2311726
Paul <paul@paulhhowells.net>
2018-03-14 17:32:26 +0000

    Injected Auth service into route to limit access according to state in Auth

--- app/routes/account-details.js
+++ app/routes/account-details.js
@@ -1,6 +1,17 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  auth: service(),
+  beforeModel() {
+    const auth = this.get('auth');
+    const isSignedIn = auth.getIsSignedIn();
+
+    if (isSignedIn === false) {
+      // Implicitly aborts the on-going transition.
+      this.transitionTo('index');
+    }
+  },
   model() {
     return this.get('store')
       .queryRecord('plan', {})
