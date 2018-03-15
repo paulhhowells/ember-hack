192e21a854f3cc8ddb36f59b83f654c905e0e8fe
Paul <paul@paulhhowells.net>
2018-03-15 17:42:29 +0000

    Set up auth-control service to use auth service methods

--- app/components/auth-control.js
+++ app/components/auth-control.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  auth: service(),
+  actions: {
+    signIn() {
+      this.get('auth').signIn();
+    },
+    signOut() {
+      this.get('auth').signOut();
+    }
+  },
 });
